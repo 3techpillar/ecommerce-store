c41539c4e295c7709722b1b79d421d84be1ad9ca
Ranvijay-Singh3170 <rvsyadav9793@gmail.com>
2025-09-18 08:02:38 +0530

    Forget password api created

--- backend/api/routes/user/user.route.js
+++ backend/api/routes/user/user.route.js
@@ -5,17 +5,28 @@ import {
   signout,
   signup,
   updateUser,
+  getUser,
+  forgotPassword,
+  verifyOtp,
+  resetPassword,
 } from "../../controllers/user/user.controller.js";
 import { verifyToken, verifyUser } from "../../utils/verifyUser.js";
 
 const router = express.Router();
 
+// Auth
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/logout", signout);
 
+// Forgot Password Flow
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-otp", verifyOtp);
+router.post("/reset-password", resetPassword);
+
+// User CRUD
 router.put("/:id", verifyUser, updateUser);
 router.get("/:id", getUserById);
-router.get("/me", verifyToken, getUserById);
+router.get("/me", verifyToken, getUser);
 
 export default router;
